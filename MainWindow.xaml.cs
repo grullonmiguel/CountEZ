62204b683bd81ba8bee0e2cb32f7d9c74ac19bb2
mgrullon <grullonmiguel@gmail.com>
2024-09-06 23:38:22 -0400

    Initial

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CountEZ
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
